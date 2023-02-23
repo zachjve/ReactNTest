61ff2e74eee7a9f6b6a9601b75fc166c473f0caf
admin <admin@iMac-ITK-07.local>
2023-02-23 11:06:32 +0100

    step 1

--- App.js
+++ App.js
@@ -4,11 +4,27 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textInput}>Hello World!</Text>
+      {sampleGoals.map((goal, index) => (
+        <Text key={index}>{goal}</Text>
+      ))}
       <StatusBar style="auto" />
     </View>
   );
 }
+
+const sampleGoals = [
+  "Faire les courses",
+  "Aller à la salle de sport 3 fois par semaine",
+  "Monter à plus de 5000m d altitude",
+  "Acheter mon premier appartement",
+  "Perdre 5 kgs",
+  "Gagner en productivité",
+  "Apprendre un nouveau langage",
+  "Faire une mission en freelance",
+  "Organiser un meetup autour de la tech",
+  "Faire un triathlon",
+];
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +32,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textInput: {
+    color: 'black',
+    fontWeight: 'bold',
   },
 });
